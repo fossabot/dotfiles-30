184ee9fd21fa88743aca0d3e03684d014d633b28
Robert Ellegate <rje6459@rit.edu>
2019-02-04 18:24:33 -0500

    Replaced aliases with proper expressions

--- install-windows-dev-apps.ps1
+++ install-windows-dev-apps.ps1
@@ -1,12 +1,12 @@
-echo Configuring Windows
+Write-Output Configuring Windows
 
 if (choco --version){
-	echo Attempting to update Chocolatey
+	Write-Output Attempting to update Chocolatey
 	choco upgrade chocolatey
 }
 else {
-	echo Installing Chocolatey
-	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	Write-Output Installing Chocolatey
+	Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 } 
 
 choco install chocolatey-packages.config -y
