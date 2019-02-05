a8272fda185e21fe4a1320bdcc03194760853ebe
Robert Ellegate (Student Employee) <rjehelp@rit.edu>
2019-02-05 10:27:25 -0500

    added symlink for vscode settings in windows

--- windows/symlink-windows.ps1
+++ windows/symlink-windows.ps1
@@ -1,2 +1,3 @@
-New-Item -Path $env:HOMEPATH\.gitaliases -ItemType SymbolicLink -Value ..\..\.gitaliases
-New-Item -Path $env:HOMEPATH\bin -ItemType SymbolicLink -Value $PWD\bin
+New-Item -Path $env:HOMEPATH\.gitaliases -ItemType SymbolicLink -Value ..\prefs\git\.gitaliases
+New-Item -Path $env:HOMEPATH\bin -ItemType SymbolicLink -Value .\bin
+New-Item -Path $env:APPDATA\Code\User\settings.json -ItemType SymbolicLink -Value ..\prefs\visual-studio-code\settings.json
