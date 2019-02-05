4cfe87d559e866defd908c093602720e422adc74
Robert Ellegate (Student Employee) <rjehelp@rit.edu>
2019-02-05 13:21:36 -0500

    Added remainder of windows .ps1 scripts to main setup script

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,5 +1,7 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 Invoke-Expression ".\symlink-windows.ps1"
-Invoke-Expression ".\windows\config-windows.ps1"
-Invoke-Expression ".\windows\install-windows-dev-apps.ps1"
+Invoke-Expression ".\config-windows.ps1"
+Invoke-Expression ".\install-windows-dev-apps.ps1"
+Invoke-Expression ".\config-git-windows.ps1"
+Invoke-Expression ".\install-windows-desktop-apps.ps1"
