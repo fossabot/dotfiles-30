565f16210a0433a927a8b7c7b62f1d961d242a3d
Robert Ellegate (Student Employee) <rjehelp@rit.edu>
2019-02-05 11:42:24 -0500

    adding Update-Help and enabling seconds in clock regedit

--- windows/config-windows.ps1
+++ windows/config-windows.ps1
@@ -3,3 +3,10 @@
     "Path",
     [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";$Env:USERPROFILE\bin",
     [EnvironmentVariableTarget]::User)
+
+# Update Powershell Help files
+Update-Help | Out-Null
+
+# Enable seconds in clock
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name 'ShowSecondsInSystemClock' -Value '1' -PropertyType DWORD -Force
+
