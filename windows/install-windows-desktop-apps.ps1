1d8435d0ef8d98389526b374ad3485843a60c1ab
Robert Ellegate (Student Employee) <rjehelp@rit.edu>
2019-02-05 13:04:17 -0500

    installing choco packages from install-*-apps.ps1 instead of from packages.config

---
+++ windows/install-windows-desktop-apps.ps1
@@ -0,0 +1,18 @@
+Write-Output Installing Windows Desktop Apps
+
+choco install -y `
+    adobereader `
+    dropbox `
+    electrum.install `
+    Firefox `
+	google-drive-file-stream `
+    GoogleChrome `
+	keepass-plugin-readablepassphrasegen `
+	keepass-plugin-keeotp `
+    keepass-yet-another-favicon-downloader `
+    keepass.install `
+    sharex `
+	signal `
+    slack `
+    teamviewer `
+    yubikey-personalization-tool
--- windows/install-windows-dev-apps.ps1
+++ windows/install-windows-dev-apps.ps1
@@ -1,4 +1,4 @@
-Write-Output Configuring Windows
+Write-Output Installing Windows Development Apps
 
 if (choco --version){
 	Write-Output Attempting to update Chocolatey
@@ -9,4 +9,24 @@ else {
 	Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 } 
 
-choco install chocolatey-packages.config -y
+choco install -y `
+	7zip.install `
+	audacity `
+	awscli `
+	ccleaner `
+	choco-upgrade-all-at-startup `
+	ffmpeg `
+	git.install `
+	golang `
+	notepadplusplus `
+	pdftk `
+	putty `
+	python `
+	sysinternals `
+	vlc `
+	vscode `
+	wireshark
+
+code --install-extension ms-vscode.powershell
+code --install-extension dotjoshjohnson.xml
+code --install-extension earshinov.sort-lines-by-selection
