5a33cfd0c79dca919673ae74275f7e296ae55c4d
Robert Ellegate (Student Employee) <rjehelp@rit.edu>
2019-02-13 16:45:55 -0500

    updating symlinks to overwrite existing files

--- windows/symlink-windows.ps1
+++ windows/symlink-windows.ps1
@@ -5,5 +5,5 @@ New-Item -Path $env:HOMEPATH\.gitaliases -ItemType SymbolicLink -Value ..\prefs\
 New-Item -Path $env:HOMEPATH\bin -ItemType SymbolicLink -Value .\bin
 
 # Adds symlinks for vscode config files in homepath
-New-Item -Path $env:APPDATA\Code\User\settings.json -ItemType SymbolicLink -Value ..\prefs\visual-studio-code\settings.json
-New-Item -Path $env:APPDATA\Code\User\keybindings.json -ItemType SymbolicLink -Value ..\prefs\visual-studio-code\keybindings.json
+New-Item -Path $env:APPDATA\Code\User\settings.json -ItemType SymbolicLink -Value ..\prefs\visual-studio-code\settings.json -Force
+New-Item -Path $env:APPDATA\Code\User\keybindings.json -ItemType SymbolicLink -Value ..\prefs\visual-studio-code\keybindings.json -Force
