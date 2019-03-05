08eb838e4b626c5f1b81278bb7c3c95b990dab8c
Robert Ellegate <robert@its-ia1202.ad.rit.edu>
2019-03-05 09:23:36 -0500

    Adding symlinks for SSH prefs

--- linux/symlink-linux.sh
+++ linux/symlink-linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 GIT_PREFS_PATH=$(cd ../prefs/git; pwd)
+SSH_PREFS_PATH=$(cd ../prefs/ssh; pwd)
 
 ln -sf $(pwd)/bin $HOME
 ln -sf $GIT_PREFS_PATH/.gitconfig $HOME/.gitconfig
@@ -8,3 +9,4 @@ ln -sf $GIT_PREFS_PATH/.gitaliases $HOME/.gitaliases
 
 mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
+ln -sf $SSH_PREFS_PATH/config $HOME/.ssh/config 
