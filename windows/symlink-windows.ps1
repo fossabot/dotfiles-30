906c58c71ea33ab44eae6a89b8fc08bdafd1a9c8
Robert Ellegate <rje6459@rit.edu>
2019-03-18 16:13:27 -0400

    Added .gitignore dirs symlink

--- windows/symlink-windows.ps1
+++ windows/symlink-windows.ps1
@@ -1,8 +1,11 @@
 # Adds symlink for .gitaliases in homepath
 New-Item -Path $env:HOMEPATH\.gitaliases -ItemType SymbolicLink -Value ..\prefs\git\.gitaliases
 
-# Adds symlink for .gitignore_global in homepath
-New-Item -Path $env:HOMEPATH\.gitignore_global -ItemType SymbolicLink -Value ..\prefs\git\.gitignore_global
+# Adds symlink for .gitignore_common.d directory in homepath
+New-Item -Path $env:HOMEPATH\.gitignore_common.d -ItemType SymbolicLink -Value ..\prefs\git\.gitignore_common.d
+
+# Adds symlink for .gitignore_languages.d directory in homepath
+New-Item -Path $env:HOMEPATH\.gitignore_languages.d -ItemType SymbolicLink -Value ..\prefs\git\.gitignore_languages.d
 
 # Adds symlink for \bin in homepath
 New-Item -Path $env:HOMEPATH\bin -ItemType SymbolicLink -Value .\bin
