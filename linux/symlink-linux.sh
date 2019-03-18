842ceb1bc28ef538a601357117897430fff8e6f4
Robert Ellegate <rje6459@rit.edu>
2019-03-18 16:12:22 -0400

    Removing .gitcong and adding .gitignore dirs

--- linux/symlink-linux.sh
+++ linux/symlink-linux.sh
@@ -4,8 +4,9 @@ GIT_PREFS_PATH=$(cd ../prefs/git; pwd)
 SSH_PREFS_PATH=$(cd ../prefs/ssh; pwd)
 
 ln -sf $(pwd)/bin $HOME
-ln -sf $GIT_PREFS_PATH/.gitconfig $HOME/.gitconfig
 ln -sf $GIT_PREFS_PATH/.gitaliases $HOME/.gitaliases
+ln -sf $GIT_PREFS_PATH/.gitignore_common.d $HOME/.gitignore_common.d
+ln -sf $GIT_PREFS_PATH/.gitignore_languages.d $HOME/.gitignore_languages.d
 
 mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
--- mac/symlink-mac.sh
+++ mac/symlink-mac.sh
@@ -3,9 +3,9 @@
 GIT_PREFS_PATH=$(cd ../prefs/git; pwd)
 
 ln -sf $(pwd)/bin $HOME
-ln -sf $GIT_PREFS_PATH/.gitconfig $HOME/.gitconfig
-ln -sf $GIT_PREFS_PATH/.gitignore_global $HOME/.gitignore_global
 ln -sf $GIT_PREFS_PATH/.gitaliases $HOME/.gitaliases
+ln -sf $GIT_PREFS_PATH/.gitignore_common.d $HOME/.gitignore_common.d
+ln -sf $GIT_PREFS_PATH/.gitignore_languages.d $HOME/.gitignore_languages.d
 
 mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
