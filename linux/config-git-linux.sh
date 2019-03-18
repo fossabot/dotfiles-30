b5124e59fdb668eb6a391f2a136e5eee9921f648
Robert Ellegate <rje6459@rit.edu>
2019-03-18 16:13:13 -0400

    Added commands to generate OS specific .gitignore

--- linux/config-git-linux.sh
+++ linux/config-git-linux.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Generates new .gitignore_global file
+cat $HOME/.gitignore_common.d/{Backup.*,GPG.*,Linux.*} > $HOME/.gitignore_global
+
 git config --global core.excludesfile $HOME/.gitignore_global
 git config --global include.path $HOME/.gitaliases
--- windows/config-git-windows.ps1
+++ windows/config-git-windows.ps1
@@ -1,1 +1,4 @@
+Get-Content $env:HOMEPATH\.gitignore_global\Backup.gitignore $env:HOMEPATH\.gitignore_global\GPG.gitignore $env:HOMEPATH\.gitignore_global\Windows.gitignore | Out-File $env:HOMEPATH\.gitignore_global
+
+git config --global core.excludesfile $env:HOMEPATH\.gitignore_global
 git config --global include.path $env:HOMEPATH\.gitaliases
