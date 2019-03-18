89c883539121fa6d8572335e9e259bf3e6b68569
Robert Ellegate <rje6459@rit.edu>
2019-03-18 16:32:17 -0400

    added commit.gpgSign true to config-git scripts

--- linux/config-git-linux.sh
+++ linux/config-git-linux.sh
@@ -5,3 +5,4 @@ cat $HOME/.gitignore_common.d/{Backup.*,GPG.*,Linux.*} > $HOME/.gitignore_global
 
 git config --global core.excludesfile $HOME/.gitignore_global
 git config --global include.path $HOME/.gitaliases
+git config --global commit.gpgSign true
