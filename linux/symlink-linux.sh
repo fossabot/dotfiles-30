1b0b93962d9f7315f9ef9feb1be5121bc5289392
Robert Ellegate <rje6459@rit.edu>
2019-03-19 08:54:22 -0400

    Added symlink for ../prefs/bash/.bashrc

--- linux/symlink-linux.sh
+++ linux/symlink-linux.sh
@@ -2,11 +2,18 @@
 
 GIT_PREFS_PATH=$(cd ../prefs/git; pwd)
 SSH_PREFS_PATH=$(cd ../prefs/ssh; pwd)
+BASH_PREFS_PATH=$(cd ../prefs/bash; pwd)
 
+# Creates a symlink for user binaries in $HOME directory
 ln -sf $(pwd)/bin $HOME
+
+# Creates symlinks for git dotfiles in $HOME directory
 ln -sf $GIT_PREFS_PATH/.gitaliases $HOME/.gitaliases
 ln -sf $GIT_PREFS_PATH/.gitignore_common.d $HOME
 ln -sf $GIT_PREFS_PATH/.gitignore_languages.d $HOME
+
+# Creates symlink for .bashrc in $HOME directory
+ln -sf $BASH_PREFS_PATH/.bashrc $HOME/.bashrc
 
 mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
