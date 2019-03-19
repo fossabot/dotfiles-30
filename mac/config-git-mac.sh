b7904f537fbd8ec37112fa161e70e19f592c2963
Robert Ellegate <rje6459@rit.edu>
2019-03-19 09:20:35 -0400

    moving common git config declarations to prefs/git/.gitconfig_common

--- mac/config-git-mac.sh
+++ mac/config-git-mac.sh
@@ -3,7 +3,4 @@
 # Generates new .gitignore_global file
 cat $HOME/.gitignore_common.d/{Backup.*,GPG.*,macOS.*} > $HOME/.gitignore_global
 
-git config --global core.excludesfile $HOME/.gitignore_global
-git config --global include.path $HOME/.gitaliases
-git config --global user.name "Robert Ellegate"
-git config --global commit.gpgSign true
+source ../prefs/git/.gitconfig_common
--- windows/config-git-windows.ps1
+++ windows/config-git-windows.ps1
@@ -1,6 +1,3 @@
 Get-Content $env:HOMEPATH\.gitignore_global\Backup.gitignore $env:HOMEPATH\.gitignore_global\GPG.gitignore $env:HOMEPATH\.gitignore_global\Windows.gitignore | Out-File $env:HOMEPATH\.gitignore_global
 
-git config --global core.excludesfile $env:HOMEPATH\.gitignore_global
-git config --global include.path $env:HOMEPATH\.gitaliases
-git config --global user.name "Robert Ellegate"
-git config --global commit.gpgSign true
+. ..\prefs\git\.gitconfig_common
