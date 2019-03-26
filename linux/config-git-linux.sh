bf7379d303a74fea8bb4f3c5d06d014bbeee1892
Robert Ellegate <rje6459@rit.edu>
2019-03-26 13:46:05 -0400

    Added config to make gpg2 the default gpg program

--- linux/config-git-linux.sh
+++ linux/config-git-linux.sh
@@ -3,4 +3,10 @@
 # Generates new .gitignore_global file
 cat $HOME/.gitignore_common.d/{Backup.*,GPG.*,Linux.*} > $HOME/.gitignore_global
 
+# Source common configurations
 source ../prefs/git/.gitconfig_common
+
+# Sets gpg2 as default gpg program if present
+if [ -x /usr/bin/gpg2 ]; then
+        git config --global gpg.program=/usr/bin/gpg2
+fi
