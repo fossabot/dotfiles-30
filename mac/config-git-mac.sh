4361091b33fa5a40dd144d65f666ebf5725bde01
Robert Ellegate <rje6459@rit.edu>
2019-06-19 13:56:46 -0400

    Adding gpg2 section to config-git-mac

--- mac/config-git-mac.sh
+++ mac/config-git-mac.sh
@@ -4,3 +4,8 @@
 cat $HOME/.gitignore_common.d/{Backup.*,GPG.*,macOS.*} > $HOME/.gitignore_global
 
 source ../prefs/git/.gitconfig_common
+
+# Sets gpg2 as default gpg program if present
+if [ -x /usr/bin/gpg2 ]; then
+        git config --global gpg.program /usr/bin/gpg2
+fi
