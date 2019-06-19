9850a1c27383ef3c6bf789588eea2cb8ac459645
Robert Ellegate <rje6459@rit.edu>
2019-06-19 10:43:25 -0400

    Adding new shell script to install programs I use

---
+++ linux/rhel/install-rhel-dev-apps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dnf install -y \
+    burp
+    chromium
+    chromium-libs-media-freeworld
+    code
+    dnf-automatic
+    docker-ce
+    docker-ce-cli
+    fail2ban
+    ffmpeg
+    fortune-mod
+    golang
+    gource
+    htop
+    nmap
+    peek
+    prename
+    ruby
+    snapd
+    vlc
